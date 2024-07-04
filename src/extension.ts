c1c3e99c775cad0e6dcb66ea359e964081bb3d95
Samuel Braun <office.samigo.a@gmail.com>
2024-07-04 15:32:59 +0200

    Fixed casing for unix

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode"
 import fs from "fs"
-import { LinkDefinitionProvider } from "./linkDefinitionProvider"
+import { LinkDefinitionProvider } from "./LinkDefinitionProvider"
 import path from "path"
 
 /**
