fd4fb5fd7ac61f3a2e7e3520cd5a079d882305ee
Samuel Braun <office.samigo.a@gmail.com>
2024-07-06 14:38:03 +0200

    Added jumpPatterns

--- src/linkDefinitionProvider.ts
+++ src/linkDefinitionProvider.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode"
 import { findConfigFile } from "./config"
 import crossSpawn from "cross-spawn"
 import path from "path"
+import fs from "fs"
 import { vscLog } from "./extension"
 
 export class LinkDefinitionProvider implements vscode.DocumentLinkProvider {
@@ -41,9 +42,46 @@ export class LinkDefinitionProvider implements vscode.DocumentLinkProvider {
       vscLog("Info", `Created ${compatibleData.links.length} Links!`)
 
       return compatibleData.links.map((link: any) => {
-        link.range = new vscode.Range(document.positionAt(link.range[0]), document.positionAt(link.range[1]))
-        link.target = vscode.Uri.parse(link.target)
-        return link
+        const finalLink: vscode.DocumentLink & {
+          _originalVsclTarget: string
+          _vsclTarget: vscode.Uri
+          _jumpPattern: any
+        } = {
+          _originalVsclTarget: link.target,
+          _vsclTarget: vscode.Uri.parse(link.target),
+          range: new vscode.Range(document.positionAt(link.range[0]), document.positionAt(link.range[1])),
+          tooltip: link.tooltip || "",
+          _jumpPattern: link.jumpPattern,
+        }
+
+        // Jumping to a specific line with "jumpPattern"
+        if (finalLink._vsclTarget.scheme === "file" && typeof link.jumpPattern === "object") {
+          const targetDocument = vscode.workspace.textDocuments.find(
+            (doc) => doc.uri.path === finalLink._vsclTarget.path,
+          )
+          if (targetDocument) {
+            if (link.jumpPattern.pattern) {
+              const regex = new RegExp(link.jumpPattern.pattern, link.jumpPattern.flags || "g")
+              const index = targetDocument.getText().search(regex)
+              if (index >= 0) {
+                const position = targetDocument.positionAt(index)
+                finalLink.target = vscode.Uri.parse(`${link.target}#L${position.line + 1}:${position.character + 1}`)
+              }
+            } else if (link.jumpPattern.literal) {
+              const index = targetDocument.getText().indexOf(link.jumpPattern.literal)
+              if (index >= 0) {
+                const position = targetDocument.positionAt(index)
+                finalLink.target = vscode.Uri.parse(`${link.target}#L${position.line + 1}:${position.character + 1}`)
+              }
+            }
+          } else {
+            return finalLink
+          }
+        } else {
+          finalLink.target = finalLink._vsclTarget
+        }
+
+        return finalLink
       })
     } catch (error) {
       vscLog("Error", error as any)
@@ -51,13 +89,86 @@ export class LinkDefinitionProvider implements vscode.DocumentLinkProvider {
 
     return null
   }
+
+  resolveDocumentLink(
+    link: vscode.DocumentLink & { _vsclTarget: vscode.Uri; _jumpPattern: any; _originalVsclTarget: string },
+  ): vscode.ProviderResult<vscode.DocumentLink> {
+    if (link.target) {
+      return {
+        ...link,
+        target: link._vsclTarget,
+      }
+    }
+
+    console.log("SUCCESS")
+
+    try {
+      const targetDocument = fs.readFileSync(link._vsclTarget.fsPath, "utf8")
+      if (link._jumpPattern.pattern) {
+        const regex = new RegExp(link._jumpPattern.pattern, link._jumpPattern.flags || "g")
+        const index = targetDocument.search(regex)
+        if (index >= 0) {
+          const { line, column } = getLineAndColumn(targetDocument, index)
+          link.target = vscode.Uri.parse(`${link._originalVsclTarget}#L${line}:${column}`)
+        }
+      } else if (link._jumpPattern.literal) {
+        const index = targetDocument.indexOf(link._jumpPattern.literal)
+        if (index >= 0) {
+          const { line, column } = getLineAndColumn(targetDocument, index)
+          link.target = vscode.Uri.parse(`${link._originalVsclTarget}#L${line}:${column}`)
+        }
+      }
+    } catch (error) {
+      vscLog("Error", error as any)
+    }
+
+    if (!link.target) {
+      vscLog("Error", `Could not find linePattern in document: ${link._vsclTarget.path}`)
+      link.target = link._vsclTarget
+    }
+
+    return link
+  }
+}
+
+function getLineAndColumn(
+  text: string,
+  index: number,
+): {
+  line: number
+  column: number
+} {
+  if (index < 0 || index > text.length) {
+    return { line: 1, column: 1 }
+  }
+
+  const lines = text.split("\n")
+  let currentLength = 0
+  let lineNumber = 1
+
+  for (const line of lines) {
+    const lineLength = line.length + 1
+    if (currentLength + lineLength > index) {
+      return {
+        line: lineNumber,
+        column: index - currentLength + 1,
+      }
+    }
+    currentLength += lineLength
+    lineNumber++
+  }
+
+  return {
+    line: lineNumber - 1,
+    column: lines[lines.length - 1].length + 1,
+  }
 }
 
 function runCli(workspacePath: string, args: string[], content: string): Promise<string | null> {
   return new Promise((resolve) => {
-    // const linkedPath = path.join("XXX\\Desktop\\vscode-links-cli\\build\\main\\cli.js")
+    const linkedPath = path.join("C:\\Users\\SBrau\\Desktop\\vscode-links-cli\\build\\main\\cli.js")
     const cliPath = path.join(workspacePath, "node_modules", "vscode-links-cli", "build", "module", "cli.js")
-    const cliProcess = crossSpawn("node", [cliPath, ...args], {
+    const cliProcess = crossSpawn("node", [linkedPath, ...args], {
       stdio: ["pipe", "pipe", "pipe"],
       cwd: workspacePath,
       env: { ...process.env },
