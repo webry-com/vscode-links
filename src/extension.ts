bf0b865bf0a195c82912f21432dc999c645ca56e
Samuel Braun <office.samigo.a@gmail.com>
2024-07-09 17:44:54 +0200

    chore: Update npm dependencies and add testVSCodeImport command

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode"
 import fs from "fs"
 import { LinkDefinitionProvider } from "./LinkDefinitionProvider"
 import path from "path"
+import { loadConfig } from "c12"
 
 /**
  * TODO: Custom search function
@@ -50,6 +51,40 @@ export function activate(context: vscode.ExtensionContext) {
         const document = await vscode.workspace.openTextDocument(filePath)
         await vscode.window.showTextDocument(document)
       }
+    }),
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-links.testVSCodeImport", async () => {
+      const workspaceFolders = vscode.workspace.workspaceFolders ?? []
+      if (workspaceFolders.length === 0) {
+        vscode.window.showErrorMessage("No workspaces are currently opened.")
+        return
+      }
+
+      const selectedWorkspaceIndex = await vscode.window.showQuickPick(
+        workspaceFolders.map((folder) => folder.name),
+        {
+          placeHolder: "Select a workspace",
+        },
+      )
+
+      if (!selectedWorkspaceIndex) {
+        vscode.window.showInformationMessage("No workspace was selected.")
+        return
+      }
+
+      const selectedWorkspace = workspaceFolders.find((folder) => folder.name === selectedWorkspaceIndex)
+      if (!selectedWorkspace) {
+        vscode.window.showErrorMessage("Failed to find the selected workspace.")
+        return
+      }
+
+      const testConfig = await loadConfig({
+        cwd: selectedWorkspace.uri.fsPath,
+        name: "vsc-links",
+      })
+      vscLog("Info", testConfig.config.links.length)
+      vscLog("Info", testConfig.config.links[0].pattern.toString())
     }),
   )
 
